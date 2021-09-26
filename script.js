c34bda1e58b39dcdb1a768257a260d9127878002
Mario Aldaco <marioat94@gmail.com>
2021-09-26 15:34:24 -0500

    Created three arrays and initialized them with phrases

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("I'm starting a new project!");
+
+let firstPhrase = ['All our dreams can come true, if we have the courage to pursue them.', 'Only the paranoid survive.', 'Everything you can imagine is real.'];
+let secondPhrase = ['The secret of getting ahead is getting started.', 'It’s hard to beat a person who never gives up.', 'Do one thing every day that scares you.'];
+let thirdPhrase = ['The best time to plant a tree was 20 years ago. The second best time is now.', 'If people are doubting how far you can go, go so far that you can’t hear them anymore.', 'It’s no use going back to yesterday, because I was a different person then.']
